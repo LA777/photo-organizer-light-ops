b033fd1191e9db540bbaa1bb7b649e4c75f7997a
Alexander Lazarenko <23220049+LA777@users.noreply.github.com>
2021-02-26 17:37:21 +0200

    added unit tests for EnumerableExtension

---
+++ Polo.UnitTests/Extensions/EnumerableExtensionTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Polo.Extensions;
+using Xunit;
+
+namespace Polo.UnitTests.Extensions
+{
+    public class EnumerableExtensionTests
+    {
+        [Fact]
+        public void IsNullOrEmpty_Should_Return_True_If_Collection_Is_Null_Test()
+        {
+            // Arrange & Act
+            var result = ((string[])null).IsNullOrEmpty();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsNullOrEmpty_Should_Return_True_If_Collection_Is_Empty_Test()
+        {
+            // Arrange
+            string[] sut = { };
+
+            // Act
+            var result = sut.IsNullOrEmpty();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsNullOrEmpty_Should_Return_False_If_Collection_Is_Not_Empty_Test()
+        {
+            // Arrange
+            string[] sut = { "hello" };
+
+            // Act
+            var result = sut.IsNullOrEmpty();
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
