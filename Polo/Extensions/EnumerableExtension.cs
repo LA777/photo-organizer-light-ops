984385910d5d6068fb628ec02c858cdbe14938a8
Alex L <LA.xdna@gmail.com>
2022-12-10 15:22:52 +0200

    micro refactor

--- Polo/Extensions/EnumerableExtension.cs
+++ Polo/Extensions/EnumerableExtension.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace Polo.Extensions
+﻿namespace Polo.Extensions
 {
     public static class EnumerableExtension
     {
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T>? collection)
         {
             return collection == null || !collection.Any();
         }
